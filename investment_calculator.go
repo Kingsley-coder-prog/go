a46ca9f9fda6c17f117de93ab521e05b6a366627
Kingsley Ifeanyi <kifeanyi216@gmail.com>
2024-05-13 12:35:08 +0100

    Improving User Input Fetches

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,12 +6,19 @@ import (
 )
 
 func main() {
-	const inflationRate = 6.5
-	var investmentAmount float64 = 1000
-	years := 10.0
+	const inflationRate = 2.5
+	var investmentAmount float64
+	var years float64
 	expectedReturnRate := 5.5
 
+	fmt.Print("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
