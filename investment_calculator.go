0bd2dd017ac0f2fccb8684c16c9be97fa407d343
Kingsley Ifeanyi <kifeanyi216@gmail.com>
2024-05-13 12:19:40 +0100

    Understanding The Importance of Variables

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,9 +7,11 @@ import (
 
 func main() {
 	const inflationRate = 6.5
-	var investmentAmount float64 = 1000.0
+	var investmentAmount float64 = 1000
 	years := 10.0
 	expectedReturnRate := 5.5
+
+	fmt.Scan(&investmentAmount)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
